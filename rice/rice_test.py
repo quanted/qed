5c613aa5803d79875403e9be12294b7343fc90d6
Chancellor T. Pascale <chancebatwalrus@gmail.com>
2013-02-04 12:58:49 -0500

    Adding beginning of unit test suite

---
+++ rice/rice_test.py
@@ -0,0 +1,19 @@
+import unittest
+
+class RiceTest(unittest.TestCase):
+    
+    def setUp(self):
+        pass
+    
+    def test_msed(self):
+        self.fail("Failing MSED test")
+        
+    def test_vw(self):
+        self.fail("Failing VW test")
+        
+    def test_mai1(self):
+        self.fail("Failing VW test")
+        
+    def test_cw(self):
+        self.fail("Failing CW test")
+    

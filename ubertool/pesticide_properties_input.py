72cdb480a1ebae037dd44eae42dbad5db5747004
Tom Purucker <purucker.tom@gmail.com>
2013-03-06 23:40:23 -0500

    fixing tab/space errors and ecosysteminput call

--- ubertool/pesticide_properties_input.py
+++ ubertool/pesticide_properties_input.py
@@ -26,9 +26,9 @@ class PPInputPage(webapp.RequestHandler):
         logger.info(cookies)
         templatepath = os.path.dirname(__file__) + '/../templates/'
         html = template.render(templatepath + '01uberheader.html', {'title':'Ubertool'})
-		html = html + template.render(templatepath + 'ubertool_pest_jquery.html', {})
+        html = html + template.render(templatepath + 'ubertool_pest_jquery.html', {})
         html = html + template.render(templatepath + '02uberintroblock_nomodellinks.html', {'title2':'Pesticide Properties', 'model':'pesticide_properties'})
-		html = html + template.render (templatepath + '03ubertext_links_left.html', {})                
+        html = html + template.render (templatepath + '03ubertext_links_left.html', {})                
         html = html + template.render(templatepath + '04uberinput_start.html', {'model':'pesticide_properties'})
         html = html + str(pesticide_properties_db.PPInp())
         html = html + template.render(templatepath + '04uberinput_end.html', {'sub_title': 'Submit'})

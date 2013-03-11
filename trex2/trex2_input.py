01599d75fb347b07a7e10b83080129d275ac335d
hongtao510 <hongtao510@gmail.com>
2013-03-11 15:43:06 -0400

    update trex2

---
+++ trex2/trex2_input.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 03 13:30:41 2012
+@author: thong
+"""
+import os
+os.environ['DJANGO_SETTINGS_MODULE']='settings'
+import cgitb
+cgitb.enable()
+import webapp2 as webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import template
+from trex import trexdb
+
+class trexInputPage(webapp.RequestHandler):
+    def get(self):
+        text_file = open('trex/trex_description.txt','r')
+        x = text_file.read()
+        templatepath = os.path.dirname(__file__) + '/../templates/'
+        html = template.render(templatepath + '01uberheader.html', {'title':'Ubertool'})
+        html = html + template.render(templatepath + '02uberintroblock_wmodellinks.html', {'model':'trex2','page':'input'})
+        html = html + template.render (templatepath + '03ubertext_links_left.html', {})        
+        html = html + template.render (templatepath + '04uberinput_start.html', {
+                'model':'trex', 
+                'model_attributes':'TREX2 Inputs'})
+        html = html + """<a href="trex_input.html" class="TREX1"> Want to Use TREX1?</a>
+
+
+        """
+        html = html + str(trexdb.trexInp())
+        html = html + template.render (templatepath + '04uberinput_end.html', {'sub_title': 'Submit'})
+        html = html + template.render (templatepath + '05ubertext_links_right.html', {})
+        html = html + template.render(templatepath + '06uberfooter.html', {'links': ''})
+        self.response.out.write(html)
+
+app = webapp.WSGIApplication([('/.*', trexInputPage)], debug=True)
+
+def main():
+    run_wsgi_app(app)
+
+if __name__ == '__main__':
+    main()
+    
+    

54349dda7e1d67469a4366df9af055aeefd5ff5e
Tom Purucker <purucker.tom@gmail.com>
2013-03-12 18:04:12 -0300

    fixing rice input page not to require user

--- rice/Ricedb.py
+++ rice/Ricedb.py
@@ -12,7 +12,10 @@ from django.utils.safestring import mark_safe
 from google.appengine.api import users
 
 class RiceInp(forms.Form):
-    user_id = users.get_current_user().user_id()
+    user = user.get_current_user()
+    user_id = ""
+    if user:
+        user_id = user.user_id()
     config_name = forms.CharField(label="Use Configuration Name", initial="use-config-%s"%user_id)        
     chemical_name = forms.CharField(widget=forms.Textarea (attrs={'cols': 20, 'rows': 2}))
     mai = forms.FloatField(required=True,label='Mass of Applied Ingredient Applied to Paddy (kg)')

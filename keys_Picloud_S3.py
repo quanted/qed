5495985dfefa414060ab6f00b54c4b36e42d8e9a
hongtao510 <hongtao510@gmail.com>
2013-04-02 17:24:42 -0400

    add key

---
+++ keys_Picloud_S3.py
@@ -0,0 +1,8 @@
+picloud_api_key='3355'
+picloud_api_secretkey='212ed160e3f416fdac8a3b71c90f3016722856b9'
+
+amazon_s3_key='AKIAIUYH5XORGJCNDZOQ'
+amazon_s3_secretkey='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'
+
+if __name__ == '__main__':
+    main()  

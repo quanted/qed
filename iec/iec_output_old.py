8d54defdf923757e6e7aa8f0932573786e186fce
Jon Flash <fake@email.com>
2013-04-24 16:37:19 -0400

    IEC with Django Templates

---
+++ iec/iec_output_old.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+
+# IEC
+import os
+os.environ['DJANGO_SETTINGS_MODULE']='settings'
+#from iec import iec_input      <---- HAS THIS BEEN DONE?  (I JUST CHANGED THE NAME)
+import webapp2 as webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import template
+import cgi
+import cgitb
+cgitb.enable()
+from iec import iec_model
+
+class IecOutputPage(webapp.RequestHandler):
+    def post(self):        
+        form = cgi.FieldStorage()   
+        LC50 = float(form.getvalue('LC50'))
+        threshold = float(form.getvalue('threshold'))
+        dose_response = float(form.getvalue('dose_response'))
+        text_file = open('iec/iec_description.txt','r')
+        x1 = text_file.read()
+        templatepath = os.path.dirname(__file__) + '/../templates/'
+        html = template.render(templatepath + '01uberheader.html', {'title':'Ubertool'})        
+        html = html + template.render(templatepath + '02uberintroblock_wmodellinks.html',  {'model':'iec','page':'output'})
+        html = html + template.render (templatepath + '03ubertext_links_left.html', {})                               
+        html = html + template.render(templatepath + '04uberoutput_start.html', {
+                'model':'iec', 
+                'model_attributes':'IEC Output'})
+        html = html + """
+        <table width="400" border="1" class="out_1">
+          <tr>
+            <th width="200" scope="col">User Inputs</th>
+            <th width="200" scope="col">Values</th>
+          </tr>
+          <tr>
+            <td>LC50 or LD50</td>
+            <td>%.2f</td>
+          </tr>          
+          <tr>
+           <td>Threshold</td>
+           <td>%.2f</td>
+          </tr>          
+          <tr>
+           <td>Slope</td>
+           <td>%.2f</td>
+          </tr>
+        </table>
+        <p>&nbsp;</p>                     
+        """%(LC50, threshold, dose_response)                   
+        html = html + """
+        <table width="400" border="1" class="out_2">
+          <tr>
+            <th width="200" scope="col">IEC Outputs</th>
+            <th width="200" scope="col">Values</th>
+          </tr>
+          <tr>
+            <td>Z Score</td>
+            <td>%.2f</td>
+          </tr>           
+          <tr>
+            <td>"F8"</td>
+            <td>%.8f</td>
+          </tr>           
+          <tr>
+            <td>Chance of Individual Effect</td>
+            <td>%.2f</td>
+          </tr>          
+        </table>
+        """%(iec_model.z_score_f(dose_response, LC50, threshold), iec_model.F8_f(dose_response, LC50, threshold), iec_model.chance_f(dose_response, LC50, threshold))
+        html = html + template.render(templatepath + 'export.html', {})
+        html = html + template.render(templatepath + '04uberoutput_end.html', {})
+        html = html + template.render(templatepath + '06uberfooter.html', {'links': ''})
+        self.response.out.write(html)
+
+app = webapp.WSGIApplication([('/.*', IecOutputPage)], debug=True)
+
+def main():
+    run_wsgi_app(app)
+
+if __name__ == '__main__':
+    main()
+
+ 
+
+     
---
+++ iec/iec_tables.py
@@ -0,0 +1,82 @@
+import numpy
+#import django
+from django.template import Context, Template
+from django.utils.safestring import mark_safe
+from iec import iec_model
+from iec import iec_parameters
+
+def getheaderiv():
+  headings = ["User Input", "Value"]
+  return headings
+
+def getheaderov():
+  headings = ["IEC Output", "Value"]
+  return headings
+
+def gethtmlrowsfromcols(data, headings):
+    columns = [data[heading] for heading in headings]
+
+    # get the length of the longest column
+    max_len = len(max(columns, key=len))
+
+    for col in columns:
+        # padding the short columns with None
+        col += [None,] * (max_len - len(col))
+
+    # Then rotate the structure...
+    rows = [[col[i] for col in columns] for i in range(max_len)]
+    return rows
+
+def getdjtemplate():
+    dj_template ="""
+    <table id="output" >
+    {# headings #}
+        <tr>
+        {% for heading in headings %}
+            <th>{{ heading }}</th>
+        {% endfor %}
+        </tr>
+    {# data #}
+    {% for row in data %}
+    <tr>
+        {% for val in row %}
+        <td>{{ val|default:'' }}</td>
+        {% endfor %}
+    </tr>
+    {% endfor %}
+    </table>
+    """
+    return dj_template
+
+def gett1data(LC50,threshold,dose_response):
+    data = { 
+        "User Input": ['LC50 or LD50', 'Threshold', 'Slope',],
+        "Value": [LC50, threshold, dose_response,],
+    }
+    return data
+
+def gett2data(z_score_f,F8_f,chance_f):
+    data = { 
+        "IEC Output": ['Z Score', '"F8"', 'Chance of Individual Effect',],
+        "Value": ['%.2f' % z_score_f,'%.2f' % F8_f,'%.2f' % chance_f, ],
+    }
+    return data
+
+ivheadings = getheaderiv()
+ovheadings = getheaderov()
+djtemplate = getdjtemplate()
+tmpl = Template(djtemplate)
+
+def table_1(ivheadings,tmpl,LC50,threshold,dose_response):
+    #table 1
+      t1data = gett1data(LC50,threshold,dose_response)
+      t1rows = gethtmlrowsfromcols(t1data,ivheadings)
+      html = html + tmpl.render(Context(dict(data=t1rows, headings=ivheadings)))
+
+def table_2(ovheadings,tmpl,z_score_f,F8_f,chance_f):
+    #table 2
+      t2data = gett2data(z_score_f,F8_f,chance_f)
+      t2rows = gethtmlrowsfromcols(t2data,ovheadings)
+      html = html + tmpl.render(Context(dict(data=t1rows, headings=ovheadings)))
+
+

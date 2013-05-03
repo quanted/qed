819422f41bb17b8b92e661f6492e6d7ed133d1dc
Jon Flash <funkswing@gmail.com>
2013-05-03 14:08:03 -0400

    Untertool Temp Setup

---
+++ landing_pypop.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 03 13:30:41 2012
+@author: jharston
+"""
+import os
+os.environ['DJANGO_SETTINGS_MODULE']='settings'
+import webapp2 as webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import template
+
+class defaultPage(webapp.RequestHandler):
+    def get(self):
+        #print os.getcwd()
+        text_file1 = open('main_description.txt','r')
+        x = text_file1.read()
+        text_file2 = open('landing_text.txt','r')
+        xx = text_file2.read()
+        templatepath = os.path.dirname(__file__) + '/templates/'                     
+        html = template.render(templatepath+'00landing_page_pop.html', {'title':'Ubertool'})
+        self.response.out.write(html)
+
+app = webapp.WSGIApplication([('/.*', defaultPage)], debug=True)
+
+def main():
+    run_wsgi_app(app)
+
+if __name__ == '__main__':
+    main()  

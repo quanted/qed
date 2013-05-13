53ec33903c373c7b89665223910e0dc64791ceec
Marcía Snyder <snydermn@gmail.com>
2013-05-13 15:41:24 -0400

    stir algorithms and sip django

--- sip/sip_output.py
+++ sip/sip_output.py
@@ -12,6 +12,7 @@ cgitb.enable()
 import sys
 sys.path.append("../sip")
 from sip import sip_model
+from sip import sip_tables
 
 class SIPExecutePage(webapp.RequestHandler):
     def post(self):
@@ -38,168 +39,44 @@ class SIPExecutePage(webapp.RequestHandler):
         html = html + template.render (templatepath + '03ubertext_links_left.html', {})                
         html = html + template.render(templatepath + '04uberoutput_start.html', {
                 'model':'sip', 
-                'model_attributes':'SIP Output'})   
-        html = html + """
-        <table border="1" class="out_1">
-        <tr>
-            <th>User Inputs</th>
-            <th>Values</th>
-            <th>Units</th>
-        </tr>
-        <tr>
-            <td>Chemical Name</td>
-            <td>%s</td>
-            <td></td>
-        </tr>
-        <tr>
-            <td>Receptor Selected</td>
-            <td>%s</td>
-            <td></td>
-        </tr>
-        <tr>
-            <td>Body Weight of Bird</td>
-            <td>%s</td>
-            <td>kg</td>
-        </tr>
-        <tr>
-            <td>Body Weight of Mammal</td>
-            <td>%s</td>
-            <td>kg</td>
-        </tr>
-        <tr>
-            <td>Solubility</td>
-            <td>%s</td>
-            <td>mg/L</td>
-        </tr>
-        <tr>
-            <td>LD<sub>50</sub></td>
-            <td>%s</td>
-            <td>mg/kg</td>
-        </tr>
-        <tr>
-            <td>Body Weight of Assessed Bird</td>
-            <td>%s</td>
-            <td>kg</td>
-        </tr>
-        <tr>
-            <td>Body Weight of Tested Bird</td>
-            <td>%s</td>
-            <td>kg</td>
-        </tr>
-        <tr>
-            <td>Body Weight of Assessed Mammal</td>
-            <td>%s</td>
-            <td>kg</td>
-        </tr>
-        <tr>
-            <td>Body Weight of Tested Mammal</td>
-            <td>%s</td>
-            <td>kg</td>
-        </tr>
-        <tr>
-            <td>Mineau Scaling Factor</td>
-            <td>%s</td>
-            <td></td>
-        </tr>
-        <tr>
-            <td>NOAEC</td>
-            <td>%s</td>
-            <td>mg/kg</td>
-        </tr>
-        <tr>
-            <td>NOAEL</td>
-            <td>%s</td>
-            <td>mg/kg</td>
-        </tr>
-        </table>
-		<br>
-        """ % (chemical_name, select_receptor, bw_bird, bw_mamm, sol, ld50, aw_bird, tw_bird, aw_mamm, tw_mamm, mineau, noaec, noael)
+                'model_attributes':'SIP Output'})     
 
-        html = html + """
-        <table border="1" class="out_2">
-        <tr>
-            <th>SIP Outputs</th>
-            <th>Mammalian Results (%s kg)</th>
-            <th></th>
-        </tr>
-        <tr>
-            <td>Parameter</td>
-            <td>Acute</td>
-            <td>Chronic</td>
-        </tr>
-        <tr>
-            <td>Upper Bound Exposure</td>
-            <td>%0.2E mg/kg-bw</td>
-            <td>%0.2E mg/kg-bw</td>
-        </tr>
-        <tr>
-            <td>Adjusted Toxicity Value</td>
-            <td>%0.2E mg/kg-bw</td>
-            <td>%0.2E mg/kg-bw</td>
-        </tr>
-        <tr>
-            <td>Ratio of Exposure to Toxicity</td>
-            <td>%0.2E</td>
-            <td>%0.2E</td>
-        </tr>
-        <tr>
-            <td>Conclusion</td>
-            <td><font color="red">%s</font></td>
-            <td><font color="red">%s</font></td>
-        </tr>
-        </table><br>
-        
-        <table border="1" class="out_3">
-        <tr>
-            <th>SIP Outputs</th>
-            <th>>Avian Results (%s kg)</th>
-            <th></th>
-        </tr>
-        <tr>
-            <td>Parameter</td>
-            <td>Acute</td>
-            <td>Chronic</td>
-        </tr>
-        <tr>
-            <td>Upper Bound Exposure</td>
-            <td>%0.2E mg/kg-bw</td>
-            <td>%0.2E mg/kg-bw</td>
-        </tr>
-        <tr>
-            <td>Adjusted Toxicity Value</td>
-            <td>%0.2E mg/kg-bw</td>
-            <td>%0.2E mg/kg-bw</td>
-        </tr>
-        <tr>
-            <td>Ratio of Exposure to Toxicity</td>
-            <td>%0.2E</td>
-            <td>%0.2E</td>
-        </tr>
-        <tr>
-            <td>Conclusion</td>
-            <td><font color="red">%s</font></td>
-            <td><font color="red">%s</font></td>
-        </tr>
-        </table>
-        """  % (aw_mamm,
-                sip_model.dose_mamm(sip_model.fw_mamm(bw_mamm),sol,bw_mamm),
-                sip_model.dose_mamm(sip_model.fw_mamm(bw_mamm),sol,bw_mamm),
-                sip_model.at_mamm(ld50,aw_mamm,tw_mamm),
-                sip_model.act(noael,tw_mamm,aw_mamm),
-                sip_model.acute_mamm(sip_model.dose_mamm(sip_model.fw_mamm(bw_mamm),sol,bw_mamm),sip_model.at_mamm(ld50,aw_mamm,tw_mamm)),
-                sip_model.chron_mamm(sip_model.dose_mamm(sip_model.fw_mamm(bw_mamm),sol,bw_mamm),sip_model.act(noael,tw_mamm,aw_mamm)),
-                sip_model.acuconm(sip_model.acute_mamm(sip_model.dose_mamm(sip_model.fw_mamm(bw_mamm),sol,bw_mamm),sip_model.at_mamm(ld50,aw_mamm,tw_mamm))),
-                sip_model.chronconm(sip_model.chron_mamm(sip_model.dose_mamm(sip_model.fw_mamm(bw_mamm),sol,bw_mamm),sip_model.act(noael,tw_mamm,aw_mamm))),
-                aw_bird,
-                sip_model.dose_bird(sip_model.fw_bird(bw_bird),sol,bw_bird), 
-                sip_model.dose_bird(sip_model.fw_bird(bw_bird),sol,bw_bird), 
-                sip_model.at_bird(ld50,aw_bird,tw_bird,mineau),
-                sip_model.det(noaec,sip_model.fi_bird(bw_bird),bw_bird),
-                sip_model.acute_bird(sip_model.dose_bird(sip_model.fw_bird(bw_bird),sol,bw_bird),sip_model.at_bird(ld50,aw_bird,tw_bird,mineau)),
-                sip_model.chron_bird(sip_model.dose_bird(sip_model.fw_bird(bw_bird),sol,bw_bird),sip_model.det(noaec,sip_model.fi_bird(bw_bird),bw_bird)),
-                sip_model.acuconb(sip_model.acute_bird(sip_model.dose_bird(sip_model.fw_bird(bw_bird),sol,bw_bird),sip_model.at_bird(ld50,aw_bird,tw_bird,mineau))),             
-                sip_model.chronconb(sip_model.chron_bird(sip_model.dose_bird(sip_model.fw_bird(bw_bird),sol,bw_bird),sip_model.det(noaec,sip_model.fi_bird(bw_bird),bw_bird))))             
-         
+
+
+        # html = html + sip_tables.table_1(chemical_name, select_receptor, bw_bird, bw_mamm, sol, ld50, aw_bird, tw_bird, aw_mamm, tw_mamm, mineau, noaec, noael)      
+
+        html = html + sip_tables.table_all(chemical_name, select_receptor, bw_bird, bw_mamm, sol, ld50, aw_bird, tw_bird, aw_mamm, tw_mamm, mineau, noaec, noael)
+
+       
+        # html = html + """
+        #  #pre-table 1
+        # <table>
+        # <tr><H3>User Inputs: Chemical Identity</H3></tr>
+        # <tr><H4>Application and Chemical Information</H4></tr>
+        # <tr></tr>
+        # </table>
+        # """
+        # pvuheadings = sip_tables.getheaderpvu()
+        # pvrheadings = sip_tables.getheaderpvr()
+        # djtemplate = sip_tables.getdjtemplate()
+        # tmpl = Template(djtemplate)
+
+        # #table 1
+        # t1data = sip_tables.gett1data(chemical_name, select_receptor, bw_bird, bw_mamm, sol, ld50, aw_bird, tw_bird, aw_mamm, tw_mamm, mineau, noaec, noael)
+        # t1rows = sip_tables.gethtmlrowsfromcols(t1data,pvuheadings)
+        # html = html + tmpl.render(Context(dict(data=t1rows, headings=pvuheadings)))
+        # html = html + """
+        #  #pre-table 2
+        # <table>
+        # <tr><H3>Outputs: Chemical Identity</H3></tr>
+        # <tr><H4>Application and Chemical Information</H4></tr>
+        # <tr></tr>
+        # </table>
+        # """
+        # #table 2
+        # t2data = sip_tables.gett2data(aw_bird, bw_bird, sol, ld50, tw_bird, mineau, noaec)
+        # t2rows = sip_tables.gethtmlrowsfromcols(t1data,pvuheadings)
+        # html = html + tmpl.render(Context(dict(data=t1rows, headings=pvuheadings)))
         html = html + template.render(templatepath + 'export.html', {})
         html = html + template.render(templatepath + '04uberoutput_end.html', {})
         html = html + template.render(templatepath + '06uberfooter.html', {'links': ''})

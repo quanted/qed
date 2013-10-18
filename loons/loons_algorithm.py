35304f6f92e03351461a7d277cacb8d6a5f07adb
hongtao510 <hongtao510@gmail.com>
2013-10-18 03:03:15 -0400

    loon alg

--- loons/loons_algorithm.py
+++ loons/loons_algorithm.py
@@ -2,7 +2,7 @@
 """
 Created on Tue Jan 03 13:30:41 2012
 
-@author: jharston
+@author: tao hong
 """
 
 import webapp2 as webapp
@@ -10,28 +10,28 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
 import os
 
-class lesliedrAlgorithmPage(webapp.RequestHandler):
+class loonsAlgorithmPage(webapp.RequestHandler):
     def get(self):
-        text_file1 = open('lesliedr/lesliedr_algorithm.txt','r')
+        text_file1 = open('loons/loons_algorithm.txt','r')
         x = text_file1.read()         
         templatepath = os.path.dirname(__file__) + '/../templates/'
         html = template.render(templatepath + '01pop_uberheader.html', {'title'})
-        html = html + template.render(templatepath + '02pop_uberintroblock_wmodellinks.html', {'model':'lesliedr','page':'algorithm'})
+        html = html + template.render(templatepath + '02pop_uberintroblock_wmodellinks.html', {'model':'loons','page':'algorithm'})
         html = html + template.render(templatepath + '03pop_ubertext_links_left.html', {})                       
         html = html + template.render(templatepath + '04uberalgorithm_start.html', {
-                'model':'lesliedr', 
-                'model_attributes':'Leslie Model with Logistic Dose Response Algorithms', 
+                'model':'loons', 
+                'model_attributes':'Loons Population Model', 
                 'text_paragraph':x})
         html = html + template.render(templatepath + '04ubertext_end.html', {})
         html = html + template.render(templatepath + '05pop_ubertext_links_right.html', {})
         html = html + template.render(templatepath + '06pop_uberfooter.html', {'links': ''})
         self.response.out.write(html)
 
-app = webapp.WSGIApplication([('/.*', lesliedrAlgorithmPage)], debug=True)
+app = webapp.WSGIApplication([('/.*', loonsAlgorithmPage)], debug=True)
 
 def main():
     run_wsgi_app(app)
 
 if __name__ == '__main__':
     main()
-    
+    

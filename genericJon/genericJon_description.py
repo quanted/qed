0d6325934777f5c4f8f7adef43565dbf16032d81
Jon Flash <funkswing@gmail.com>
2013-10-23 09:59:47 -0400

    EPA Skinning - Description Page

--- genericJon/genericJon_description.py
+++ genericJon/genericJon_description.py
@@ -11,21 +11,19 @@ import os
 
 class genericJonDescriptionPage(webapp.RequestHandler):
     def get(self):
-        text_file1 = open('genericJon/genericJon_description.txt','r')
-        x = text_file1.read()
         text_file2 = open('genericJon/genericJon_text.txt','r')
         xx = text_file2.read()
         templatepath = os.path.dirname(__file__) + '/../templates/'
-        html = template.render(templatepath + '01hh_uberheaderJon.html', {'title':'Ubertool'})
-        html = html + template.render(templatepath + '02hh_uberintroblock_wmodellinksJon.html', {'model':'genericJon','page':'description'})
+        html = template.render(templatepath + '01uberheaderEPA.html', {'title':'Ubertool'})
+        html = html + template.render(templatepath + '02uberintroblock_wmodellinksEPA.html', {'model':'genericJon','page':'description'})
         html = html + template.render(templatepath + '03hh_ubertext_links_leftJon.html', {})                       
-        html = html + template.render(templatepath + '04ubertext_startJon.html', {
+        html = html + template.render(templatepath + '04ubertext_start.html', {
                 'model_page':'#',
                 'model_attributes':"Jon's Testing Domain", 
                 'text_paragraph':xx})
-        html = html + template.render(templatepath + '04ubertext_endJon.html', {})
-        html = html + template.render(templatepath + '05hh_ubertext_links_rightJon.html', {})
-        html = html + template.render(templatepath + '06hh_uberfooter.html', {'links': ''})
+        html = html + template.render(templatepath + '04ubertext_end.html', {})
+        html = html + template.render(templatepath + '05ubertext_links_right.html', {})
+        html = html + template.render(templatepath + '06uberfooter.html', {'links': ''})
         self.response.out.write(html)
 
 app = webapp.WSGIApplication([('/.*', genericJonDescriptionPage)], debug=True)
---
+++ stylesheets/scripts_header_epa.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  $('#topheader_p p, .logreg').hide();
+  $('.index_link').click(function(e) {
+    e.preventDefault();
+    var destination = $(this).attr('href');
+    setTimeout(function() { window.location.href = destination; }, 500);
+    $('#topheader_pic, #topheader_p').animate({
+      height:'240px'
+    }, 500);
+    $('#topheader_p p, .logreg').fadeIn(500);
+  });
+  $('.articles a, .articles_input a, articles_output a').hover(function(){
+    $(this).stop().animate({ color:'#356697' },500);
+  }, function(){
+    $(this).stop().animate({ color:'#4289AA' },500);
+  });
+  var browserWidth = $(window).width();
+  var browserHeight = $(window).height();
+  var winleft = (browserWidth / 2) - 220 + "px";
+  var wintop = (browserHeight / 2) - 30 + "px";
+  $("a[class^='fadeQAQC']").click(function () {
+    $.blockUI({
+      css:{ "top":""+wintop+"", "left":""+winleft+"", "padding": "30px 20px", "width": "400px", "height": "60px", "border": "0 none", "border-radius": "4px", "-webkit-border-radius": "4px", "-moz-border-radius": "4px", "box-shadow": "3px 3px 15px #333", "-webkit-box-shadow": "3px 3px 15px #333", "-moz-box-shadow": "3px 3px 15px #333" },
+      message: '<h2 class="popup_header">Processing Request...</h2><br/><img src="/images/loader.gif" style="margin-top:-16px">'
+    });
+  });
+});

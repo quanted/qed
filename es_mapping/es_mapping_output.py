0a88d2bdb568e4c350c1ddd4fff87eccca143c28
hongtao510 <hongtao510@gmail.com>
2013-12-05 16:00:18 -0500

    clean es_mapping

--- es_mapping/es_mapping_output.py
+++ es_mapping/es_mapping_output.py
@@ -18,33 +18,11 @@ class ESOutputPage(webapp.RequestHandler):
     def post(self):
         form = cgi.FieldStorage()   
 
-        # NSF = form.getvalue('NSF')
-        # NSP = form.getvalue('NSP')
-        # NSM = form.getvalue('NSM')
-        # Crop = form.getvalue('Crop')
-        # Pesticide = form.getvalue('Pesticide')
-
-        # IUCN_Amphibians = form.getvalue('IUCN_Amphibians')
-        # IUCN_Birds = form.getvalue('IUCN_Birds')
-        # IUCN_Mammals = form.getvalue('IUCN_Mammals')
-        # IUCN_Mammals_Marine = form.getvalue('IUCN_Mammals_Marine')
-        # IUCN_Coral = form.getvalue('IUCN_Coral')
-        # IUCN_Reptiles = form.getvalue('IUCN_Reptiles')
-        # IUCN_Seagrasses = form.getvalue('IUCN_Seagrasses')
-        # IUCN_SeaCucumbers = form.getvalue('IUCN_SeaCucumbers')        
-        # IUCN_Mangrove = form.getvalue('IUCN_Mangrove')
-        # IUCN_MarineFish = form.getvalue('IUCN_MarineFish')
-
-        # USFWS_p = form.getvalue('USFWS_p')
-        # USFWS_l = form.getvalue('USFWS_l')
-        # print "USFWS_l=", USFWS_l
-
         args={}
         for key in form:
             args[key] = form.getvalue(key)
         es_obj = es_mapping_model.es_mapping(args)
         logger.info(vars(es_obj))
-
 
         templatepath = os.path.dirname(__file__) + '/../templates/'
         ChkCookie = self.request.cookies.get("ubercookie")
@@ -55,25 +33,6 @@ class ESOutputPage(webapp.RequestHandler):
             'model':'es_mapping', 
             'model_attributes':'Endangered Species Mapper Output'})
         html = html + es_mapping_tables.table_all(es_obj)
-
-        # html = html + template.render(templatepath+'ManykmlDropbox_test.html', {
-        #        'NSF':NSF,
-        #        'NSP':NSP,
-        #        'NSM':NSM,
-        #        'Crop':Crop,
-        #        'Pesticide':Pesticide,
-        #        'IUCN_Amphibians':IUCN_Amphibians,
-        #        'IUCN_Birds':IUCN_Birds,
-        #        'IUCN_Mammals':IUCN_Mammals,
-        #        'IUCN_Mammals_Marine':IUCN_Mammals_Marine,
-        #        'IUCN_Coral':IUCN_Coral,
-        #        'IUCN_Reptiles':IUCN_Reptiles,
-        #        'IUCN_Seagrasses':IUCN_Seagrasses,
-        #        'IUCN_SeaCucumbers':IUCN_SeaCucumbers,
-        #        'IUCN_Mangrove':IUCN_Mangrove,
-        #        'IUCN_MarineFish':IUCN_MarineFish,
-        #        'USFWS_p':USFWS_p,
-        #        'USFWS_l':USFWS_l})
         html = html + template.render(templatepath + '04uberoutput_end.html', {})
         html = html + template.render(templatepath + '06uberfooter.html', {'links': ''})
         self.response.out.write(html)

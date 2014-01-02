01607f2268f95f99d40b983317a49230925a78f8
Mingshu Wang <mswang@uga.edu>
2014-01-02 13:20:59 -0500

    earthworm output updated

--- earthworm/earthworm_tables.py
+++ earthworm/earthworm_tables.py
@@ -10,7 +10,6 @@ from google.appengine.ext.webapp import template
 
 logger = logging.getLogger("earthwormTables")
 
-
 def timestamp():
     ts = time.time()
     st = datetime.datetime.fromtimestamp(ts).strftime('%A, %Y-%B-%d %H:%M:%S')
@@ -24,61 +23,67 @@ def timestamp():
     </div>"""
     return html
 
+def getheaderpvu():
+    headings = ["Parameter", "Value", "Units"]
+    return headings    
+
+def gethtmlrowsfromcols(data, headings):
+    columns = [data[heading] for heading in headings]
+
+    # get the length of the longest column
+    max_len = len(max(columns, key=len))
+
+    for col in columns:
+        # padding the short columns with None
+        col += [None,] * (max_len - len(col))
+
+    # Then rotate the structure...
+    rows = [[col[i] for col in columns] for i in range(max_len)]
+    return rows
+
+def getdjtemplate():
+    dj_template ="""
+    <table class="out_">
+    {# headings #}
+        <tr>
+        {% for heading in headings %}
+            <th>{{ heading }}</th>
+        {% endfor %}
+        </tr>
+    {# data #}
+    {% for row in data %}
+    <tr>
+        {% for val in row %}
+        <td>{{ val|default:'' }}</td>
+        {% endfor %}
+    </tr>
+    {% endfor %}
+    </table>
+    """
+    return dj_template
+
+def earthwormoutput(earthworm_obj):
+    data = { 
+        "Parameter": ['Chemical concentration in earthworm tissue',],
+        "Value": ['%.5e' %earthworm_obj.earthworm_fugacity_out,],
+        
+        "Units": ['g/kg',],
+    }
+    return data
+
+
+pvuheadings = getheaderpvu()
+djtemplate = getdjtemplate()
+tmpl = Template(djtemplate)
+
+
+
 def table_all(earthworm_obj):
     table1_out = table_1()
     table2_out = table_2(earthworm_obj)
     templatepath = os.path.dirname(__file__) + '/../templates/'
     html_all = table1_out + table2_out 
     return html_all
-
-# def getheaderpvu():
-#     headings = ["Parameter", "Value", "Units"]
-#     return headings    
-
-# def gethtmlrowsfromcols(data, headings):
-#     columns = [data[heading] for heading in headings]
-
-#     # get the length of the longest column
-#     max_len = len(max(columns, key=len))
-
-#     for col in columns:
-#         # padding the short columns with None
-#         col += [None,] * (max_len - len(col))
-
-#     # Then rotate the structure...
-#     rows = [[col[i] for col in columns] for i in range(max_len)]
-#     return rows
-
-# def getdjtemplate():
-#     dj_template ="""
-#     <table class="out_">
-#     {# headings #}
-#         <tr>
-#         {% for heading in headings %}
-#             <th>{{ heading }}</th>
-#         {% endfor %}
-#         </tr>
-#     {# data #}
-#     {% for row in data %}
-#     <tr>
-#         {% for val in row %}
-#         <td>{{ val|default:'' }}</td>
-#         {% endfor %}
-#     </tr>
-#     {% endfor %}
-#     </table>
-#     """
-#     return dj_template
-
-
-# def earthwormoutput(earthworm_obj):
-#     data = { 
-#         "Parameter": ['Chemical concentration in earthworm tissue',],
-#         "Value": ['%.5e' % %earthworm_obj.earthworm_fugacity_out,],
-        
-#         "Units": ['g/kg',],
-#     }
-#     return data
 
 def table_1():
     html = """<H3 class="out_3 collapsible" id="section1"><span></span>User Inputs</H3>
@@ -109,15 +114,16 @@ def table_1():
                 </script>"""
     return html
 
-
 def table_2(earthworm_obj):
-    html = """
-            <H3 class="out_3 collapsible" id="section1"><span></span>Earthworm Fugacity Modeling Output</H3>
-            <div class="out_3">
-                <H4 class="out_4 collapsible" id="section1"><span></span></H4>
-                    <div id="map">%s</div>
-            </div>
-           """%earthworm_obj.earthworm_fugacity_out
-    return html
-
-
+        html = """
+            <H4 class="out_4 collapsible" id="section2"><span></span>Earthworm Fugacity Modeling Output</H4>
+                <div class="out_ container_output">
+        """
+        t2data = earthwormoutput(earthworm_obj)
+        t2rows = gethtmlrowsfromcols(t2data,pvuheadings)
+        html = html + tmpl.render(Context(dict(data=t2rows, headings=pvuheadings)))
+        html = html + """
+                </div>
+        </div>
+        """
+        return html

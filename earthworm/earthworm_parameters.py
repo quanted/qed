38d2d3995a12f1985218b8a89884c2463e76fbb7
Mingshu Wang <mswang@uga.edu>
2014-01-06 10:18:49 -0500

    earthworm batch

--- earthworm/earthworm_parameters.py
+++ earthworm/earthworm_parameters.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jan 17 14:50:59 2012
-
-@author: JHarston
-"""
 import os
 os.environ['DJANGO_SETTINGS_MODULE']='settings'
 from django import forms
@@ -16,7 +10,7 @@ class earthwormInp(forms.Form):
 	k_ow = forms.FloatField(required = True, label = mark_safe('Octanol to water partition coefficient K<sub>OW</sub>'),initial = 57544)
 	l_f_e = forms.FloatField(required = True, label = 'Lipid fraction of earthworm L', initial = 0.01)
 	c_s = forms.FloatField(required = True, label = mark_safe('Chemical concentration in soil C<sub>S</sub> (mol/m<sup>3</sup>)'),initial = 0.055)
-	k_d = forms.FloatField(required = True, label = mark_safe('Soil partitioning coefficient K<sub>d</sub>'), initial = 69)
+	k_d = forms.FloatField(required = True, label = mark_safe('Soil partitioning coefficient K<sub>d</sub> (cm<sup>3</sup>/g)'), initial = 69)
 	p_s = forms.FloatField(required = True, label = mark_safe('Bulk density of soil &#961;<sub>s</sub> (g/cm<sup>3</sup>)'), initial = 1.7)
 	c_w = forms.FloatField(required = True, label = mark_safe('Chemical concentration in pore water of soil C<sub>W</sub> (mol/m<sup>3</sup>)'), initial = 0.000447)
 	m_w = forms.FloatField(required = True, label = mark_safe('Molecular weight of chemical MW (g/mol)'), initial = 406.9)

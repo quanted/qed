d0b2074ec1b00f7ff027b755474aecead0712376
Mingshu Wang <mswang@uga.edu>
2014-01-07 11:08:45 -0500

    ESMapper OpenLayer begins

---
+++ es_mapping_openlayer/es_mapping_openlayer_input.py
@@ -0,0 +1,37 @@
+'''
+Created on May 23, 2012
+
+@author: th
+'''
+import os
+os.environ['DJANGO_SETTINGS_MODULE']='settings'
+import webapp2 as webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.webapp import template
+from django import forms
+from es_mapping import es_mapping_parameters
+
+
+class esInputPage(webapp.RequestHandler):
+    def get(self):
+        templatepath = os.path.dirname(__file__) + '/../templates/'
+        html = template.render(templatepath + '01uberheader.html', {'title':'Ubertool'})
+        html = html + template.render(templatepath + 'mapper-jQuery.html', {})
+        html = html + template.render(templatepath + '02uberintroblock_wmodellinks.html', {'model':'es_mapping','page':'input'})
+        html = html + template.render (templatepath + '03ubertext_links_left.html', {})        
+        html = html + template.render (templatepath + '04uberinput_start.html', {
+                'model':'es_mapping', 
+                'model_attributes':'Endangered Species Mapper Inputs'})
+        html = html + str(es_mapping_parameters.esInp())
+        html = html + template.render (templatepath + '04uberinput_end.html', {'sub_title': 'Submit'})
+        html = html + template.render (templatepath + '05ubertext_tooltips_right.html', {})
+        html = html + template.render(templatepath + '06uberfooter.html', {'links': ''})
+        self.response.out.write(html)
+
+app = webapp.WSGIApplication([('/.*', esInputPage)], debug=True)
+
+def main():
+    run_wsgi_app(app)
+
+if __name__ == '__main__':
+    main()

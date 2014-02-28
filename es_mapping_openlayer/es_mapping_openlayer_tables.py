93b47c738a60d2da3783ac67213c56bf0b49eaa3
Mingshu Wang <mswang@uga.edu>
2014-02-28 17:26:06 -0500

    Openlayer_Birds updated

--- es_mapping_openlayer/es_mapping_openlayer_tables.py
+++ es_mapping_openlayer/es_mapping_openlayer_tables.py
@@ -25,23 +25,27 @@ def table_all(es_obj):
     table2_out = table_2()
     templatepath = os.path.dirname(__file__) + '/../templates/'
     html_plot = template.render(templatepath + 'ESMapper_OpenLayer.html', {
-               'NSF': es_obj.NSF,
-               'NSP': es_obj.NSP,
-               'NSM': es_obj.NSM,
+               'Birds': es_obj.Birds,
+               'BirdsAll':es_obj.BirdsAll,
+               'BirdsIUCN':es_obj.BirdsIUCN,
+               'BirdsUSFWS':es_obj.BirdsUSFWS,
+               # 'NSP': es_obj.NSP,
+               # 'NSM': es_obj.NSM,
                'Crop': es_obj.Crop,
                'Pesticide': es_obj.Pesticide,
-               'IUCN_Amphibians': es_obj.IUCN_Amphibians,
-               'IUCN_Birds': es_obj.IUCN_Birds,
-               'IUCN_Mammals': es_obj.IUCN_Mammals,
-               'IUCN_Mammals_Marine': es_obj.IUCN_Mammals_Marine,
-               'IUCN_Coral': es_obj.IUCN_Coral,
-               'IUCN_Reptiles': es_obj.IUCN_Reptiles,
-               'IUCN_Seagrasses': es_obj.IUCN_Seagrasses,
-               'IUCN_SeaCucumbers': es_obj.IUCN_SeaCucumbers,
-               'IUCN_Mangrove': es_obj.IUCN_Mangrove,
-               'IUCN_MarineFish': es_obj.IUCN_MarineFish,
-               'USFWS_p': es_obj.USFWS_p,
-               'USFWS_l': es_obj.USFWS_l})
+               # 'IUCN_Amphibians': es_obj.IUCN_Amphibians,
+               # 'IUCN_Birds': es_obj.IUCN_Birds,
+               # 'IUCN_Mammals': es_obj.IUCN_Mammals,
+               # 'IUCN_Mammals_Marine': es_obj.IUCN_Mammals_Marine,
+               # 'IUCN_Coral': es_obj.IUCN_Coral,
+               # 'IUCN_Reptiles': es_obj.IUCN_Reptiles,
+               # 'IUCN_Seagrasses': es_obj.IUCN_Seagrasses,
+               # 'IUCN_SeaCucumbers': es_obj.IUCN_SeaCucumbers,
+               # 'IUCN_Mangrove': es_obj.IUCN_Mangrove,
+               # 'IUCN_MarineFish': es_obj.IUCN_MarineFish,
+               # 'USFWS_p': es_obj.USFWS_p,
+               # 'USFWS_l': es_obj.USFWS_l
+               })
 
 
     html_all = timestamp() + table1_out + table2_out + html_plot
@@ -54,14 +58,6 @@ def table_1():
                 </div>"""
     return html
 
-# def table_2():
-#     html = """
-#             <H3 class="out_3 collapsible" id="section1"><span></span>Endangered Species Mapper</H3>
-#             <div class="out_3">
-#                 <H4 class="out_4 collapsible" id="section1"><span></span></H4>
-#                     <div id="map"></div>
-#             </div>
-#            """
 def table_2():
     html = """
             <H3 class="out_3 collapsible" id="section1"><span></span>Endangered Species Mapper</H3>

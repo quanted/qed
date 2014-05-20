0b34b3f060195ba9cf2ad0ed55f40033b3b84ee7
hongtao510 <hongtao510@gmail.com>
2014-05-20 16:30:17 -0400

    add rest api models

--- REST/REST_EC2/bottle_ec2.py
+++ REST/REST_EC2/bottle_ec2.py
@@ -22,6 +22,13 @@ bottle.BaseRequest.MEMFILE_MAX = 1024 * 1024 # (or whatever you want)
 host_ip=boto.utils.get_instance_metadata()['local-ipv4']
 print host_ip
 
+class NumPyArangeEncoder(json.JSONEncoder):
+    def default(self, obj):
+        import numpy as np
+        if isinstance(obj, np.ndarray):
+            return obj.tolist() # or map(int, obj)
+        return json.JSONEncoder.default(self, obj)
+
 from pymongo import Connection
 connection = Connection(host_ip, 27017)
 db = connection.ubertool
@@ -31,6 +38,215 @@ def check(user, passwd):
         return True
     return False
 all_result = {}
+
+##################################terrplant#############################################
+@route('/terrplant/<jid>', method='POST') 
+@auth_basic(check)
+def terrplant_rest(jid):
+    for k, v in request.json.iteritems():
+        exec '%s = v' % k
+        # print k, v
+    all_result.setdefault(jid,{}).setdefault('status','none')
+
+    from terrplant_rest import terrplant_model_rest
+    result = terrplant_model_rest.terrplant(version_terrplant,run_type,A,I,R,D,nms,lms,nds,lds,chemical_name,pc_code,use,application_method,application_form,solubility)
+    if (result):
+        all_result[jid]['status']='done'
+        all_result[jid]['input']=request.json
+        all_result[jid]['result']=result
+
+    return {'user_id':'admin', 'result': result.__dict__, '_id':jid}
+##################################terrplant#############################################
+
+##################################sip#############################################
+@route('/sip/<jid>', method='POST') 
+@auth_basic(check)
+def sip_rest(jid):
+    for k, v in request.json.iteritems():
+        exec '%s = v' % k
+    all_result.setdefault(jid,{}).setdefault('status','none')
+    from sip_rest import sip_model_rest
+    result = sip_model_rest.sip(chemical_name, bw_bird, bw_quail, bw_duck, bwb_other, bw_rat, bwm_other, b_species, m_species, bw_mamm, sol, ld50_a, ld50_m, aw_bird, mineau, aw_mamm, noaec, noael)
+    if (result):
+        all_result[jid]['status']='done'
+        all_result[jid]['input']=request.json
+        all_result[jid]['result']=result
+    return {'user_id':'admin', 'result': result.__dict__, '_id':jid}
+##################################sip#############################################
+
+##################################stir#############################################
+@route('/stir/<jid>', method='POST') 
+@auth_basic(check)
+def stir_rest(jid):
+    for k, v in request.json.iteritems():
+        exec '%s = v' % k
+    all_result.setdefault(jid,{}).setdefault('status','none')
+    from stir_rest import stir_model_rest
+    result = stir_model_rest.stir(run_type,chemical_name,application_rate,column_height,spray_drift_fraction,direct_spray_duration, 
+                                  molecular_weight,vapor_pressure,avian_oral_ld50,body_weight_assessed_bird,body_weight_tested_bird,mineau_scaling_factor, 
+                                  mammal_inhalation_lc50,duration_mammal_inhalation_study,body_weight_assessed_mammal,body_weight_tested_mammal, 
+                                  mammal_oral_ld50)
+    if (result):
+        all_result[jid]['status']='done'
+        all_result[jid]['input']=request.json
+        all_result[jid]['result']=result
+    return {'user_id':'admin', 'result': result.__dict__, '_id':jid}
+##################################sip#############################################
+
+##################################dust#############################################
+@route('/dust/<jid>', method='POST') 
+@auth_basic(check)
+def dust_rest(jid):
+    for k, v in request.json.iteritems():
+        exec '%s = v' % k
+    all_result.setdefault(jid,{}).setdefault('status','none')
+    from dust_rest import dust_model_rest
+    result = dust_model_rest.dust(chemical_name, label_epa_reg_no, ar_lb, frac_pest_surface, dislodge_fol_res, bird_acute_oral_study, bird_study_add_comm,
+                                  low_bird_acute_ld50, test_bird_bw, mineau_scaling_factor, mamm_acute_derm_study, mamm_study_add_comm, mam_acute_derm_ld50, mam_acute_oral_ld50, test_mam_bw)
+    if (result):
+        all_result[jid]['status']='done'
+        all_result[jid]['input']=request.json
+        all_result[jid]['result']=result
+    return {'user_id':'admin', 'result': result.__dict__, '_id':jid}
+##################################sip#############################################
+
+##################################trex2#############################################
+@route('/trex2/<jid>', method='POST') 
+@auth_basic(check)
+def trex2_rest(jid):
+    for k, v in request.json.iteritems():
+        exec '%s = v' % k
+    all_result.setdefault(jid,{}).setdefault('status','none')
+    from trex2_rest import trex2_model_rest
+    result = trex2_model_rest.trex2(chem_name, use, formu_name, a_i, Application_type, seed_treatment_formulation_name, seed_crop, seed_crop_v, r_s, b_w, p_i, den, h_l, n_a, ar_lb, day_out,
+                                    ld50_bird, lc50_bird, NOAEC_bird, NOAEL_bird, aw_bird_sm, aw_bird_md, aw_bird_lg, 
+                                    Species_of_the_tested_bird_avian_ld50, Species_of_the_tested_bird_avian_lc50, Species_of_the_tested_bird_avian_NOAEC, Species_of_the_tested_bird_avian_NOAEL, 
+                                    tw_bird_ld50, tw_bird_lc50, tw_bird_NOAEC, tw_bird_NOAEL, x, ld50_mamm, lc50_mamm, NOAEC_mamm, NOAEL_mamm, aw_mamm_sm, aw_mamm_md, aw_mamm_lg, tw_mamm,
+                                    m_s_r_p)
+    if (result):
+        result_json = json.dumps(result.__dict__, cls=NumPyArangeEncoder)
+        all_result[jid]['status']='done'
+        all_result[jid]['input']=request.json
+        all_result[jid]['result']=result
+    return {'user_id':'admin', 'result':result_json, '_id':jid}
+##################################trex2#############################################
+
+##################################therps#############################################
+@route('/therps/<jid>', method='POST') 
+@auth_basic(check)
+def therps_rest(jid):
+    for k, v in request.json.iteritems():
+        exec '%s = v' % k
+    all_result.setdefault(jid,{}).setdefault('status','none')
+    from therps_rest import therps_model_rest
+    result = therps_model_rest.therps(chem_name, use, formu_name, a_i, h_l, n_a, i_a, a_r, avian_ld50, avian_lc50, avian_NOAEC, avian_NOAEL, 
+                                      Species_of_the_tested_bird_avian_ld50, Species_of_the_tested_bird_avian_lc50, Species_of_the_tested_bird_avian_NOAEC, Species_of_the_tested_bird_avian_NOAEL,
+                                      bw_avian_ld50, bw_avian_lc50, bw_avian_NOAEC, bw_avian_NOAEL,
+                                      mineau_scaling_factor, bw_herp_a_sm, bw_herp_a_md, bw_herp_a_lg, wp_herp_a_sm, wp_herp_a_md, 
+                                      wp_herp_a_lg, c_mamm_a, c_herp_a)
+    if (result):
+        result_json = json.dumps(result.__dict__, cls=NumPyArangeEncoder)
+        all_result[jid]['status']='done'
+        all_result[jid]['input']=request.json
+        all_result[jid]['result']=result
+    return {'user_id':'admin', 'result':result_json, '_id':jid}
+##################################therps#############################################
+
+##################################iec#############################################
+@route('/iec/<jid>', method='POST') 
+@auth_basic(check)
+def iec_rest(jid):
+    for k, v in request.json.iteritems():
+        exec '%s = v' % k
+    all_result.setdefault(jid,{}).setdefault('status','none')
+    from iec_rest import iec_model_rest
+    result = iec_model_rest.iec(dose_response, LC50, threshold)
+    if (result):
+        all_result[jid]['status']='done'
+        all_result[jid]['input']=request.json
+        all_result[jid]['result']=result
+    return {'user_id':'admin', 'result': result.__dict__, '_id':jid}
+##################################iec#############################################
+
+##################################agdrift#############################################
+@route('/agdrift/<jid>', method='POST') 
+@auth_basic(check)
+def agdrift_rest(jid):
+    for k, v in request.json.iteritems():
+        exec '%s = v' % k
+    all_result.setdefault(jid,{}).setdefault('status','none')
+    from agdrift_rest import agdrift_model_rest
+    result = agdrift_model_rest.agdrift(drop_size, ecosystem_type, application_method, boom_height, orchard_type, application_rate, distance, aquatic_type, calculation_input, init_avg_dep_foa, avg_depo_gha, avg_depo_lbac, deposition_ngL, deposition_mgcm, nasae, y, x, express_y)
+    if (result):
+        all_result[jid]['status']='done'
+        all_result[jid]['input']=request.json
+        all_result[jid]['result']=result
+    return {'user_id':'admin', 'result': result.__dict__, '_id':jid}
+##################################agdrift#############################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##################################earthworm#############################################
+@route('/earthworm/<jid>', method='POST') 
+@auth_basic(check)
+def earthworm_rest(jid):
+    for k, v in request.json.iteritems():
+        exec '%s = v' % k
+    all_result.setdefault(jid,{}).setdefault('status','none')
+    from earthworm_rest import earthworm_model_rest
+    result = earthworm_model_rest.earthworm(k_ow, l_f_e, c_s, k_d, p_s, c_w, m_w, p_e)
+    if (result):
+        all_result[jid]['status']='done'
+        all_result[jid]['input']=request.json
+        all_result[jid]['result']=result
+    return {'user_id':'admin', 'result': result.__dict__, '_id':jid}
+##################################earthworm#############################################
+
+##################################rice#############################################
+@route('/rice/<jid>', method='POST') 
+@auth_basic(check)
+def rice_rest(jid):
+    for k, v in request.json.iteritems():
+        exec '%s = v' % k
+    all_result.setdefault(jid,{}).setdefault('status','none')
+    from rice_rest import rice_model_rest
+    result = rice_model_rest.rice(chemical_name, mai, dsed, a, pb, dw, osed, kd)
+    if (result):
+        all_result[jid]['status']='done'
+        all_result[jid]['input']=request.json
+        all_result[jid]['result']=result
+    return {'user_id':'admin', 'result': result.__dict__, '_id':jid}
+##################################rice#############################################
+
+##################################kabam#############################################
+@route('/kabam/<jid>', method='POST') 
+@auth_basic(check)
+def kabam_rest(jid):
+    for k, v in request.json.iteritems():
+        exec '%s = v' % k
+    all_result.setdefault(jid,{}).setdefault('status','none')
+    from kabam_rest import kabam_model_rest
+    result = kabam_model_rest.kabam(chemical_name, l_kow, k_oc, c_wdp, water_column_EEC, c_wto, mineau_scaling_factor, x_poc, x_doc, c_ox, w_t, c_ss, oc, k_ow, Species_of_the_tested_bird, bw_quail, bw_duck, bwb_other, avian_ld50, avian_lc50, avian_noaec, m_species, bw_rat, bwm_other, mammalian_ld50, mammalian_lc50, mammalian_chronic_endpoint, lf_p_sediment, lf_p_phytoplankton, lf_p_zooplankton, lf_p_benthic_invertebrates, lf_p_filter_feeders, lf_p_small_fish, lf_p_medium_fish, mf_p_sediment, mf_p_phytoplankton, mf_p_zooplankton, mf_p_benthic_invertebrates, mf_p_filter_feeders, mf_p_small_fish, sf_p_sediment, sf_p_phytoplankton, sf_p_zooplankton, sf_p_benthic_invertebrates, sf_p_filter_feeders, ff_p_sediment, ff_p_phytoplankton, ff_p_zooplankton, ff_p_benthic_invertebrates, beninv_p_sediment, beninv_p_phytoplankton, beninv_p_zooplankton, zoo_p_sediment, zoo_p_phyto, s_lipid, s_NLOM, s_water, v_lb_phytoplankton, v_nb_phytoplankton, v_wb_phytoplankton, wb_zoo, v_lb_zoo, v_nb_zoo, v_wb_zoo, wb_beninv, v_lb_beninv, v_nb_beninv, v_wb_beninv, wb_ff, v_lb_ff, v_nb_ff, v_wb_ff, wb_sf, v_lb_sf, v_nb_sf, v_wb_sf, wb_mf, v_lb_mf, v_nb_mf, v_wb_mf, wb_lf, v_lb_lf, v_nb_lf, v_wb_lf, kg_phytoplankton, kd_phytoplankton, ke_phytoplankton, mo_phytoplankton, mp_phytoplankton, km_phytoplankton, km_zoo, k1_phytoplankton, k2_phytoplankton, k1_zoo, k2_zoo, kd_zoo, ke_zoo, k1_beninv, k2_beninv, kd_beninv, ke_beninv, km_beninv, k1_ff, k2_ff, kd_ff, ke_ff, km_ff, k1_sf, k2_sf, kd_sf, ke_sf, km_sf, k1_mf, k2_mf, kd_mf, ke_mf, km_mf, k1_lf, k2_lf, kd_lf, ke_lf, km_lf, rate_constants, s_respire, phyto_respire, zoo_respire, beninv_respire, ff_respire, sfish_respire, mfish_respire, lfish_respire)
+    if (result):
+        result_json = json.dumps(result.__dict__, cls=NumPyArangeEncoder)
+        all_result[jid]['status']='done'
+        all_result[jid]['input']=request.json
+        all_result[jid]['result']=result
+    return {'user_id':'admin', 'result':result_json, '_id':jid}
+##################################kabam#############################################
+
 
 ###############geneec####################
 @route('/geneec/<jid>', method='POST')

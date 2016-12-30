e65fba2e433b22bf84605e92d9d3e52319d1708f
Tom Purucker <purucker.tom@gmail.com>
2016-12-30 00:31:25 -0500

    cleanup

--- chance/code_quality/eco/eco_code_quality.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sh $1/eco/rice/rice_code_quality.sh $2/rice $1/eco/rice/rice_code_quality.html
-sh $1/eco/iec/iec_code_quality.sh $2/rice $1/eco/rice/rice_code_quality.html
--- chance/code_quality/eco/iec/iec_code_quality.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-pylint $1 -f html > $2
-#pylint /Users/pascact1/ubertool_src/iec -f html > /Users/pascact1/ubertool_src/code_quality/eco/iec/iec_code_quality.html

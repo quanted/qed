1fdb1c8c004a91728e833011b31662dd5e535229
Tom Purucker <purucker.tom@gmail.com>
2017-01-04 15:42:50 -0500

    urls.py with includes

---
+++ urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import include, url
+
+urlpatterns = [
+    url(r'^$', include('splash_app.urls')),
+    url(r'^cts/', include('cts_app.urls')),
+    url(r'^cyan/', include('cyan_app.urls')),
+    url(r'^hem/', include('hem_app.urls')),
+    url(r'^hms/', include('hms_app.urls')),
+    url(r'^hwbi/', include('hwbi_app.urls')),
+    url(r'^pisces/', include('pisces_app.urls')),
+    url(r'^pop/', include('pop_app.urls')),
+    url(r'^ubertool/', include('ubertool_app.urls')),
+]

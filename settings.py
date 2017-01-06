3b0e3e7bda6b4690d85a1861f747d189382ed2fb
Tom Purucker <purucker.tom@gmail.com>
2017-01-05 23:31:23 -0500

    rolling back cts

--- settings.py
+++ settings.py
@@ -23,11 +23,21 @@ TEMPLATE_ROOT = os.path.join(PROJECT_ROOT, 'templates_qed/') #.replace('\\','/')
 #STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static_qed')
 #os.path.join(PROJECT_ROOT, 'templates_qed')
 
+# CTS- Boolean for if it's on Nick's local machine or not..
+NICK_LOCAL = False
+
 # Define ENVIRONMENTAL VARIABLES
 os.environ.update({
     'PROJECT_PATH': PROJECT_ROOT,
     'SITE_SKIN': 'EPA',                          # Leave empty ('') for default skin, 'EPA' for EPA skin
     'CONTACT_URL': 'https://www.epa.gov/research/forms/contact-us-about-epa-research',
+    'CTS_TEST_SERVER': 'http://134.67.114.6:8080',
+    'CTS_EPI_SERVER': 'http://134.67.114.8',
+    'CTS_JCHEM_SERVER': 'http://134.67.114.2',
+    'CTS_EFS_SERVER': 'http://134.67.114.2',
+    'CTS_SPARC_SERVER': 'http://204.46.160.69:8080',
+# http://n2626ugath802:8080/sparc-integration/rest/calc/multiProperty
+    'CTS_VERSION': '1.5.0'
 })
 
 # SECURITY WARNING: we keep the secret key in a shared dropbox directory
@@ -73,6 +83,9 @@ TEMPLATES = [
 # Application definition
 INSTALLED_APPS = (
     'splash_app',
+    'qed_cts',
+    #'cts_api',
+    #'cts_testing',
     # 'django.contrib.admin',
     # 'django.contrib.auth',
     #'django.contrib.contenttypes',
@@ -83,6 +96,9 @@ INSTALLED_APPS = (
     # 'docs',
     # 'rest_framework_swagger'
 )
+
+# This breaks the pattern of a "pluggable" TEST_CTS django app, but it also makes it convenient to describe the server hosting the TEST API.
+TEST_CTS_PROXY_URL = "http://10.0.2.2:7080/"
 
 MIDDLEWARE_CLASSES = (
     # 'django.contrib.sessions.middleware.SessionMiddleware',
@@ -167,6 +183,9 @@ print('TEMPLATE_ROOT = %s' %TEMPLATE_ROOT)
 
 DOCS_ROOT = os.path.join(PROJECT_ROOT, 'docs', '_build', 'html')
 DOCS_ACCESS = 'public'
+
+NODEJS_HOST = '134.67.114.1'
+NODEJS_PORT = None
 
 # Log to console in Debug mode
 if DEBUG:
--- urls.py
+++ urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import include, url
 
+#regular expressions
+# the r in r'^cts/index.html$' indicates that what is inside the quotes is a regular expression
+# the ^ in r'^cts/index.html$' indicates that we are looking to extend from the root dir from this part of the string
+# the $ in r'^cts/index.html$' indicates that we are looking to extend end the mathing part exactly here
+
 urlpatterns = [
     url(r'^$', include('splash_app.urls')),
-    #url(r'^cts/', include('cts_app.urls')),
+    #url(r'^cts/', include('qed_cts.urls')),
     #url(r'^cyan/', include('cyan_app.urls')),
     #url(r'^hem/', include('hem_app.urls')),
     #url(r'^hms/', include('hms_app.urls')),

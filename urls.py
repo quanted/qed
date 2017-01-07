a96cdad7a3794870e91590d33f9ebba147b60d05
Tom Purucker <purucker.tom@gmail.com>
2017-01-07 02:24:43 -0500

    urls and submodule for ubertool_app update

--- urls.py
+++ urls.py
@@ -5,14 +5,18 @@ from django.conf.urls import include, url
 # the ^ in r'^cts/index.html$' indicates that we are looking to extend from the root dir from this part of the string
 # the $ in r'^cts/index.html$' indicates that we are looking to extend end the mathing part exactly here
 
+print('qed.urls')
+#appends to the list of url patterns to check against
 urlpatterns = [
     url(r'^$', include('splash_app.urls')),
-    #url(r'^cts/', include('qed_cts.urls')),
-    #url(r'^cyan/', include('cyan_app.urls')),
-    #url(r'^hem/', include('hem_app.urls')),
-    #url(r'^hms/', include('hms_app.urls')),
-    #url(r'^hwbi/', include('hwbi_app.urls')),
-    #url(r'^pisces/', include('pisces_app.urls')),
-    #url(r'^pop/', include('pop_app.urls')),
-    url(r'^ubertool/', include('ubertool_app.urls')),
+    #url(r'^cts', include('qed_cts.urls')),
+    #url(r'^cyan', include('cyan_app.urls')),
+    #url(r'^hem', include('hem_app.urls')),
+    #url(r'^hms', include('hms_app.urls')),
+    #url(r'^hwbi', include('hwbi_app.urls')),
+    #url(r'^pisces', include('pisces_app.urls')),
+    #url(r'^pop', include('pop_app.urls')),
+    #url(r'^sam', include('sam_app.urls')),
+    url(r'^ubertool', include('ubertool_app.urls')),
+    #url(r'^untertool', include('unter_app.urls')),
 ]

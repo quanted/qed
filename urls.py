f39d94c2d328827d74c6ec0b9d9607efdad39421
Tom Purucker <purucker.tom@gmail.com>
2017-01-08 15:10:59 -0500

    ubertool_app routing and app updates

--- urls.py
+++ urls.py
@@ -8,15 +8,15 @@ from django.conf.urls import include, url
 print('qed.urls')
 #appends to the list of url patterns to check against
 urlpatterns = [
-    url(r'^$', include('splash_app.urls')),
-    #url(r'^cts', include('qed_cts.urls')),
-    #url(r'^cyan', include('cyan_app.urls')),
-    #url(r'^hem', include('hem_app.urls')),
-    #url(r'^hms', include('hms_app.urls')),
-    #url(r'^hwbi', include('hwbi_app.urls')),
-    #url(r'^pisces', include('pisces_app.urls')),
-    #url(r'^pop', include('pop_app.urls')),
-    #url(r'^sam', include('sam_app.urls')),
-    url(r'^ubertool', include('ubertool_app.urls')),
-    #url(r'^untertool', include('unter_app.urls')),
+    url(r'^', include('splash_app.urls')),
+    #url(r'^cts'/, include('qed_cts.urls')),
+    #url(r'^cyan/', include('cyan_app.urls')),
+    #url(r'^hem/', include('hem_app.urls')),
+    #url(r'^hms/', include('hms_app.urls')),
+    #url(r'^hwbi/', include('hwbi_app.urls')),
+    #url(r'^pisces/', include('pisces_app.urls')),
+    #url(r'^pop/', include('pop_app.urls')),
+    #url(r'^sam/', include('sam_app.urls')),
+    url(r'^ubertool/', include('ubertool_app.urls')),
+    #url(r'^untertool/', include('unter_app.urls')),
 ]

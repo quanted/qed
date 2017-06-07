1417887525248ed733f2a6539e977267ea99b80a
kurtw555 <kurt.wolfe@gmail.com>
2017-06-07 11:07:51 -0400

    Added pisces_db back to settings

--- settings.py
+++ settings.py
@@ -129,6 +129,14 @@ DATABASES = {
     'hwbi_db': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(PROJECT_ROOT, 'hwbi_app/hwbi_db.sqlite3'),
+    'pisces_db': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pisces',
+        'USER': 'cgifadmin',
+        'PASSWORD': 'Ptfocns17!cgi5',
+        'HOST': '172.20.100.15',
+        'PORT': '5432',
+    }
     }
 }
 

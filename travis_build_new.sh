b50f77c60972555323f8205f4b68cb3a9b5146d4
dbsmith88 <dbsmith@uga.edu>
2018-07-20 13:37:15 -0500

    travis packages install test

---
+++ travis_build_new.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+sudo apt-get update
+if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
+  wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
+else
+  wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+fi
+
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+hash -r
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+conda info -a
+
+conda create -q -n travis-env python=3.6 --yes --file requirements_qed/requirements.txt
+source activate travis-env
+python setup.py install

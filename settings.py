db765c8741435c49b8fb58fab128435182ded617
Jeff Minucci <jminucci@uga.edu>
2019-02-21 16:52:56 -0500

    move setting of env vars to settings.py

--- settings.py
+++ settings.py
@@ -9,6 +9,11 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from temp_config.set_environment import DeployEnv
+
+# Determine env vars to use:
+runtime_env = DeployEnv()
+runtime_env.load_deployment_environment()
 
 print('settings.py')
 

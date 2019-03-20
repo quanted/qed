05e22f3f2b7c5489c75e5e2458702bb8e8fc0935
popenc <i.nickpope@gmail.com>
2019-03-20 19:50:00 -0400

    added password wall on app-by-app basis to login_middleware.py

--- login_middleware.py
+++ login_middleware.py
@@ -23,54 +23,43 @@ class RequireLoginMiddleware:
 	"""
 	def __init__(self, get_response):
 		self.get_response = get_response
-		# self.require_login_path = getattr(settings, 'REQUIRE_LOGIN_PATH', '/login')
 		self.login_url = re.compile(settings.REQUIRE_LOGIN_PATH)
 		self.username = "qeduser"
 		self.hashed_pass = None
-
-		# # good place to read in hashed password from file??
+		self.apps_with_password = ["cts"]
 		try:
 			_hash_pass = open('secrets/secret_key_login.txt', 'r')  # read in hashed password from file
 			self.hashed_pass = _hash_pass.read().encode('utf-8')  # encode for bcrypt
 		except Exception as e:
 			logging.warning("Exception reading hashed password from file..")
 			self.hashed_pass = None
-	
 
 	def __call__(self, request):
-
 		response = self.get_response(request)
 		return response
 
-
 	def process_view(self, request, view_func, view_args, view_kwargs):
-
 		assert hasattr(request, 'user')
-
 		path = request.path
-
+		redirect_path = request.POST.get('next', "")
+		if not self.needs_password(path + redirect_path):
+			return
 		if not request.user.is_authenticated:
-			
 			if not self.login_url.match(path):
 				return redirect('{}?next={}'.format(settings.REQUIRE_LOGIN_PATH, path))
-
 			if request.POST and self.login_url.match(path):
 				return self.login_auth(request)
 
-
-	# def process_request(self, request):
-
-	# 	# _already_authenticated == ''
-
-	# 	if request.user.is_authenticated:
-	# 		return  # user already authenticated
-
-	# 	if request.POST:
-	# 		return self.login_auth(request)
-
-	# 	elif request.path != self.require_login_path and request.user.is_anonymous():
-	# 		return HttpResponseRedirect('{}?next={}'.format(self.require_login_path, request.path))
-
+	def needs_password(self, path):
+		"""
+		Checks requested path against apps that need a
+		password wall. Returns True if path has app name
+		in it that needs password protected.
+		"""
+		for app in self.apps_with_password:
+			if app in path:
+				return True
+		return False
 
 	def login_auth(self, request):
 
@@ -81,17 +70,14 @@ class RequireLoginMiddleware:
 		# redirect if hashed pw unable to be set, or user didn't enter password:
 		if not self.hashed_pass or not password:
 			return redirect('/login?next={}'.format(next_page))
-
 		# check if username is correct:
 		if username != self.username:
 			logging.warning("username {} incorrect..".format(username))
 			return redirect('/login?next={}'.format(next_page))
-
 		# check if password is correct:
 		if not bcrypt.checkpw(password.encode('utf-8'), self.hashed_pass):
 			logging.warning("password incorrect for user: {}".format(username))
 			return redirect('/login?next={}'.format(next_page))
-
 		# Add user to django db if not already there:
 		if not User.objects.filter(username=username).exists():
 			_user = User.objects.create_user(username, 'email@address.com', password)
@@ -113,39 +99,26 @@ class RequireLoginMiddleware:
 
 
 
-
-
-
 #######################################################################################
 ################################ User Login Pages #####################################
 #######################################################################################
 
 def login(request):
-
 	next_page = request.GET.get('next')
-
 	html = render_to_string('01epa_drupal_header.html', {
 		'SITE_SKIN': os.environ['SITE_SKIN'],
 		'TITLE': u"Q.E.D."
 	})
 	html += render_to_string('02epa_drupal_header_bluestripe_onesidebar.html', {})
 	html += render_to_string('03epa_drupal_section_title_splash.html', {})
-
 	html += render_to_string('06ubertext_start_index_drupal.html', {
 		'TITLE': 'User Login',
 		'TEXT_PARAGRAPH': ""
 	})
 	html += render_to_string('07ubertext_end_drupal.html', {})
-	# html += links_left.ordered_list()  # fills out 05ubertext_links_left_drupal.html
-
 	html += render_to_string('login_prompt.html', {'next': next_page}, request=request)
-
-	# scripts and footer
 	html += render_to_string('09epa_drupal_ubertool_css.html', {})
-	# html += render_to_string('09epa_drupal_ubertool_scripts.html', {})
 	html += render_to_string('10epa_drupal_footer.html', {})
-
 	response = HttpResponse()
 	response.write(html)
-
 	return response

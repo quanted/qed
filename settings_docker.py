d0e4177a9860eb3425e3ea735625b88bcdc8fc50
popenc <i.nickpope@gmail.com>
2021-06-25 15:28:48 -0400

    reverts STATIC_ROOT back to 'collected_static'

--- settings_docker.py
+++ settings_docker.py
@@ -243,17 +243,17 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_ROOT = os.path.join('src', 'collected_static')
-# STATIC_ROOT = os.path.join('collected_static')
+# STATIC_ROOT = os.path.join('src', 'collected_static')
+STATIC_ROOT = os.path.join('collected_static')
 HMS_ANGULAR_APP_DIR = "/src/static_qed/hms/webapp"
 HMS_ANGULAR_APP_ASSETS_DIR = "/src/static_qed/hms/webapp/assets"
-CYANWEB_ANGULAR_APP_DIR = "/src/static_qed/epa-cyano-web"
+# CYANWEB_ANGULAR_APP_DIR = "/src/static_qed/epa-cyano-web"
 
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static_qed'),
     # os.path.join(PROJECT_ROOT, 'collected_static'),
     HMS_ANGULAR_APP_DIR,
-    CYANWEB_ANGULAR_APP_DIR
+    # CYANWEB_ANGULAR_APP_DIR
 )
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
